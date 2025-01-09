67bd9ba6dbf21384dabde7208931c33dc9e05125
Wake1st <36935801+Wake1st@users.noreply.github.com>
2025-01-08 19:26:08 -0700

    Wake1st/timeline generation (#7)
    * initial planning
    
    * more notes
    
    * inciting incident generation
    
    * file reorganizing
    
    * added weapon sub-clue type
    
    * setting up innocent alibis
    
    * cleanup
    
    * added innocent alibi occurences
    
    * adding primary nodes
    
    * initial crimeline creation

---
+++ timeline_generation/GENERATION_PROCESS.md
@@ -0,0 +1,35 @@
+# Generation Process
+This is a step by step walkthrough to better understand how the timeline generation works.
+
+## Notes
+- In order to generate `Occurence`s which make sense
+  - there must be a set list of `Action`s performed upon `Clue`s, some being `Location` bound
+    - EX: player can "burry" a `Clue` in the "garden", or "burn" a `Clue` in the "living room" fireplace
+  - `Suspect`s will change `Location` and take `Action` based on their `Motive`
+    - EX: "Zhou" will falsify an "alibi" `Clue` (`Testimony`?) for "O'Leary" out of "love"
+    - EX: "Zhou" will plant a fake "item" `Clue` near the body out of "revenage" against "Beuregard"
+  - it needs to be easy and logical to understand when all the pieces are in place
+  - it would be best to start with something hand crafted to see how the pieces might be automated best
+
+
+## Steps
+- [x] start with a root node: 
+  - [x] someone murdered someone
+  - [x] somewhere
+  - [x] with some item
+  - [x] everyone else should be accounted for during that time range
+    - [x] give each person a location
+    - [x] set a time that intersects the murder time
+    - [x] create occurences (possibly shared) for the timeline
+- [x] add primary nodes (everyone was somewhere - "dinner", "lounging", "found the body"... for a long duration of time)
+- [x] add minor nodes
+  - [x] go backwards from root node, then forwards from root node
+  - [ ] switch between people, allowing a chance for an `Occurence` should a person's path cross another
+    - [ ] "meeting"/"seeing"
+    - [ ] "observing"/"overhearing"
+  - [x] these should connect with the primary nodes when appropriate
+    - ~~[ ] if they need to change `Location`, then insert the appropriate `Location` changes from one place to another~~
+- [ ] once someone "raises the alarm", everyone should either:
+  - [ ] go to the "body"
+  - [ ] take advantage of the "panic"
+- [ ] a `Suspect`'s `Location` and `Action` will be drive by their `Motive`
---
+++ timeline_generation/TimelineGenerator.cs
@@ -0,0 +1,308 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+using Godot.Collections;
+
+public partial class TimelineGenerator : Node
+{
+  public Array<Person> Persons = new()
+  {
+    GD.Load<Person>("res://resources/persons/dr_isaac_zhou.tres"),
+    GD.Load<Person>("res://resources/persons/major_thomas_oleary.tres"),
+    GD.Load<Person>("res://resources/persons/sam_beauregard.tres"),
+    GD.Load<Person>("res://resources/persons/sir_geoffrey_bradford.tres"),
+  };
+
+  public Array<Location> Locations = new()
+  {
+    GD.Load<Location>("res://resources/locations/dining_room.tres"),
+    GD.Load<Location>("res://resources/locations/foyer.tres"),
+    GD.Load<Location>("res://resources/locations/garden.tres"),
+    GD.Load<Location>("res://resources/locations/kitchen.tres"),
+    GD.Load<Location>("res://resources/locations/library.tres"),
+    GD.Load<Location>("res://resources/locations/living_room.tres"),
+  };
+
+  public Array<Weapon> Weapons = new()
+  {
+    GD.Load<Weapon>("res://resources/clues/weapons/bust.tres"),
+    GD.Load<Weapon>("res://resources/clues/weapons/candelabra.tres"),
+    GD.Load<Weapon>("res://resources/clues/weapons/cleaver.tres"),
+    GD.Load<Weapon>("res://resources/clues/weapons/fire_poker.tres"),
+    GD.Load<Weapon>("res://resources/clues/weapons/paper_weight.tres"),
+    GD.Load<Weapon>("res://resources/clues/weapons/shears.tres"),
+  };
+
+  public CrimeLine Generate()
+  {
+    CrimeLine crimeLine = new();
+
+    (
+      Occurence incitingIncident,
+      Person victim,
+      Person killer,
+      TimeRange killTime,
+      int primeSceneIndex
+    ) = GenerateIncitingIncident();
+
+    crimeLine.RootNode = incitingIncident;
+
+    //  next, the other persons must be accounted for during the kill time
+    Array<Person> innocents = Persons.Duplicate();
+    innocents.Remove(victim);
+    innocents.Remove(killer);
+    Array<Occurence> alibiOccurences = GenerateAlibis(innocents, killTime, primeSceneIndex);
+
+    //  next, primary nodes (dinner)
+    Occurence primaryIncident = GeneratePrimaryIncident(killTime, victim);
+
+    // fill in nodes for each person
+    crimeLine.SuspectTimelines = GenerateSuspectTimelines(primaryIncident, incitingIncident, alibiOccurences, primeSceneIndex);
+
+    return crimeLine;
+  }
+
+  private (Occurence, Person, Person, TimeRange, int) GenerateIncitingIncident()
+  {
+    Random rand = new();
+
+    //  first, there must be an inciting incident
+    Person victim = Persons.GetRandFromArray();
+    Person killer = Persons.GetRandFromArray(Persons.IndexOf(victim));
+
+    Location primeScene = Locations.GetRandFromArray();
+    int primeSceneIndex = Locations.IndexOf(primeScene);
+
+    int weaponIndex = rand.Next(Weapons.Count);
+    Weapon weapon = Weapons[weaponIndex];
+
+    KillAction killAction = new()
+    {
+      Actor = killer,
+      Victim = victim,
+      Type = weapon.KillType
+    };
+
+    TimeRange killTime = GenerateKillTime(rand);
+
+    Occurence incitingIncident = new()
+    {
+      When = killTime,
+      Where = primeScene,
+      Action = killAction,
+    };
+
+    return (incitingIncident, victim, killer, killTime, primeSceneIndex);
+  }
+
+  private TimeRange GenerateKillTime(Random rand)
+  {
+    int startHour = rand.Next(1, 25);
+    int endHour = rand.Next(startHour, 25);
+    int startMinutes = rand.Next(12) * 5;
+    int endMinutes = startMinutes + 5;
+
+    return new TimeRange()
+    {
+      Start = new TimePoint()
+      {
+        Hour = startHour,
+        Minute = startMinutes
+      },
+      End = new TimePoint()
+      {
+        Hour = endHour,
+        Minute = endMinutes
+      }
+    };
+  }
+
+  private Array<Occurence> GenerateAlibis(Array<Person> innocents, TimeRange killTime, int primeSceneIndex)
+  {
+    System.Collections.Generic.Dictionary<Location, List<Tuple<Person, TimeRange>>> alibiLocations = new();
+    Random rand = new();
+
+    foreach (Person innocent in innocents)
+    {
+      //  set a time that intersects the killTime
+      TimeRange timeRange = killTime.GenerateIntersectingRange();
+
+      //  give each person a location
+      Location alibiLocation = Locations.GetRandFromArray(primeSceneIndex);
+
+      //  add the person to the dictionary
+      if (alibiLocations.ContainsKey(alibiLocation))
+      {
+        alibiLocations[alibiLocation].Add(
+          new Tuple<Person, TimeRange>(innocent, timeRange)
+        );
+      }
+      else
+      {
+        alibiLocations.Add(
+          alibiLocation,
+          new List<Tuple<Person, TimeRange>>() { new(innocent, timeRange) }
+        );
+      }
+    }
+
+    //  create occurences for the innocents
+    Array<Occurence> occurences = new();
+    foreach (Location location in alibiLocations.Keys)
+    {
+      var list = alibiLocations[location];
+      if (list.Count > 1)
+      {
+        //  multiple people involved
+        var randItem = list[rand.Next(list.Count)];
+        list.Remove(randItem);
+        GroupAction groupAction = new()
+        {
+          Actor = randItem.Item1,
+          Type = GetRandEnum<GroupActionType>(),
+          Others = list.Select(tuple => tuple.Item1).AsArray()
+        };
+        foreach (var personTimeRange in list)
+        {
+          occurences.Add(new Occurence
+          {
+            When = personTimeRange.Item2,
+            Where = location,
+            Action = groupAction
+          });
+        }
+      }
+      else
+      {
+        //  just one person
+        var personTimeRange = list.First();
+        occurences.Add(new Occurence
+        {
+          When = personTimeRange.Item2,
+          Where = location,
+          Action = new LoneAction
+          {
+            Actor = personTimeRange.Item1,
+            Type = GetRandEnum<LoneActionType>(),
+          }
+        });
+      }
+    }
+
+    return occurences;
+  }
+
+  private T GetRandEnum<[MustBeVariant] T>()
+  {
+    Random rand = new();
+    System.Array values = Enum.GetValues(typeof(T));
+    return (T)values.GetValue(rand.Next(values.Length));
+  }
+
+  private Occurence GeneratePrimaryIncident(TimeRange killTime, Person victim)
+  {
+    Array<Person> others = Persons.Duplicate();
+    others.Remove(victim);
+
+    return new Occurence
+    {
+      When = killTime.GenerateDisconnectingRange(),
+      Where = Locations.GetRandFromArray(),
+      Action = new GroupAction
+      {
+        Actor = victim,
+        Type = GetRandEnum<GroupActionType>(),
+        Others = others,
+      }
+    };
+  }
+
+  private Godot.Collections.Dictionary<Person, Timeline> GenerateSuspectTimelines(Occurence primaryIncident, Occurence incitingIncident, Array<Occurence> alibiOccurences, int primeSceneIndex)
+  {
+    Random rand = new();
+    Godot.Collections.Dictionary<Person, Timeline> timelines = new();
+
+    foreach (Person person in Persons)
+    {
+      Timeline timeline = new();
+      Occurence alibi = alibiOccurences.First(o => person == o.Action.Actor);
+      alibi ??= alibiOccurences.First(o => ((GroupAction)o.Action).Others.Contains(person));
+
+      //  generate minor occurences (person is alone)
+      Array<Occurence> beforeNodes = new();
+      Array<TimeRange> timeRanges = primaryIncident.When.GenerateInsertingRanges(alibi.When);
+      foreach (TimeRange range in timeRanges)
+      {
+        beforeNodes.Add(new Occurence
+        {
+          When = range,
+          Where = Locations.GetRandFromArray(primeSceneIndex),
+          Action = new LoneAction
+          {
+            Actor = person,
+            Type = GetRandEnum<LoneActionType>()
+          }
+        });
+      }
+
+      //  add an after occurence
+      Occurence afterNode = new()
+      {
+        When = alibi.When.GenerateDisconnectingRange(before: false),
+        Where = Locations.GetRandFromArray(primeSceneIndex),
+        Action = new LoneAction
+        {
+          Actor = person,
+          Type = GetRandEnum<LoneActionType>()
+        }
+      };
+
+      //  add the occurences
+      timeline.Nodes = new Array<Occurence>
+      {
+        primaryIncident,
+      };
+      timeline.Nodes.AddRange(beforeNodes);
+      timeline.Nodes.Add(alibi);
+      timeline.Nodes.Add(afterNode);
+
+      timelines.Add(person, timeline);
+    }
+
+    return timelines;
+  }
+}
+
+public static class GeneratorExtensions
+{
+  public static T GetRandFromArray<[MustBeVariant] T>(this Array<T> items, int? exceptionIndex = null)
+  {
+    Random rand = new();
+
+    if (exceptionIndex != null)
+    {
+      int itemIndex;
+      do
+      {
+        itemIndex = rand.Next(items.Count);
+      } while (itemIndex == exceptionIndex);
+
+      return items[itemIndex];
+    }
+    else
+    {
+      return items[rand.Next(items.Count)];
+    }
+  }
+
+  public static Array<T> AsArray<[MustBeVariant] T>(this IEnumerable<T> items)
+  {
+    Array<T> arrayItems = new();
+    foreach (T item in items)
+    {
+      arrayItems.Add(item);
+    }
+    return arrayItems;
+  }
+}
